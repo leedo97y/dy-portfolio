dfa940877f468bb0f60e8b8749afe9b40b0a1d4b
leedo97y <dlehdus1433@gmail.com>
2024-07-03 20:30:50 +0900

    fix: 프로젝트, 소개 부분 수정
    - 프로필 사진 스타일 수정
    - 소개 텍스트 수정
    - 소개 markdown 수정
    - 프로젝트 상세 설명 markdown 수정
    - react-type-animation 라이브러리 추가

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -4,6 +4,7 @@ import ContactItem from "../ContactItem";
 import Introduce from "./Introduce";
 
 import { DataProps } from "@/types";
+import { TypeAnimation } from "react-type-animation";
 
 const Information = ({ information }: Pick<DataProps, "information">) => {
   return (
@@ -12,19 +13,29 @@ const Information = ({ information }: Pick<DataProps, "information">) => {
         <div className="relative flex shrink gap-8 items-center">
           {information.imgSrc && (
             <div className="hidden md:flex relative w-40 shrink-0 h-40 items-center justify-around">
-              <Image
-                className="w-[100%] h-[100%] rounded-full border-r-2 border-l-2 border-t-2 border-b-2 border-gray-600"
-                src={information.imgSrc}
-                height={100}
-                width={100}
-                alt="img"
-              />
+              <div className="w-[100%] h-[100%] p-[3px] items-center justify-center rounded-full bg-gradient-to-tl from-GRADIENT_FROM via-PRIMARY to-GRADIENT_TO">
+                <Image
+                  className="w-[100%] h-[100%] rounded-full"
+                  src={information.imgSrc}
+                  height={100}
+                  width={100}
+                  alt="img"
+                />
+              </div>
             </div>
           )}
         </div>
         <div className="flex flex-col gap-2">
           <h1 className="leading-[1.15]">
-            안녕하세요,
+            <TypeAnimation
+              sequence={["안녕하세요,", 1000, "소통하며 사고하는,", 1000]}
+              wrapper="span"
+              speed={1}
+              style={{ display: "inline-block" }}
+              repeat={0}
+              cursor={false}
+              preRenderFirstString
+            />
             <br /> 프론트엔드 개발자{" "}
             <span className="text-PRIMARY font-semibold">{information.name}</span>
             입니다.

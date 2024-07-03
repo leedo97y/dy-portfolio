d582302522d45bd20d9740e767f056bb775cf538
leedo97y <dlehdus1433@gmail.com>
2024-07-03 21:38:03 +0900

    feat: GA 세팅 코드 추가
    - GA 세팅을 위한 .env 파일 및 gtag 파일 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,26 +2,31 @@ import "@/styles/globals.css";
 import { DefaultSeo } from "next-seo";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Script from "next/script";
+
+import * as gtag from "../lib/gtag";
 
 /**
  * @description SEO를 위해 본인의 정보로 수정해주세요.
  */
 const DEFAULT_SEO = {
-  title: "Front-End 개발자 이도연 입니다.",
+  title: "이도연 | Front-End 개발자",
   description: "안녕하세요, 프론트엔드 개발자 이도연입니다.",
   canonical: "https://www.naver.com/",
   openGraph: {
     type: "website",
     locale: "ko_KR",
     url: "https://www.naver.com/",
-    title: "Front-End 개발자 이도연 입니다.",
-    site_name: "Front-End 개발자 이도연 입니다.",
+    title: "이도연 | Front-End 개발자",
+    site_name: "이도연 | Front-End 개발자",
     images: [
       {
         url: "/share.png",
         width: 285,
         height: 167,
-        alt: "Front-End 개발자 이도연 입니다.",
+        alt: "이도연 | Front-End 개발자",
       },
     ],
   },
@@ -34,11 +39,11 @@ const DEFAULT_SEO = {
   additionalMetaTags: [
     {
       name: "application-name",
-      content: "Front-End 개발자 이도연 입니다.",
+      content: "이도연 | Front-End 개발자",
     },
     {
       name: "msapplication-tooltip",
-      content: "Front-End 개발자 이도연 입니다.",
+      content: "이도연 | Front-End 개발자",
     },
     {
       name: "viewport",
@@ -47,9 +52,43 @@ const DEFAULT_SEO = {
   ],
 };
 
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
+
 const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_TRACKING_ID}', {
+          page_path: window.location.pathname,
+        });
+      `,
+        }}
+      />
       <DefaultSeo {...DEFAULT_SEO} />
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
